2492d6c3fa3cc95f4e8f9f61b415f352ec1db213
Jhuliana <jholeanasantos@gmail.com>
2022-07-11 13:34:22 -0300

    fix: get purchases

--- src/controllers/purchasesController.js
+++ src/controllers/purchasesController.js
@@ -1,8 +1,11 @@
 import db from "../db.js";
 
 export async function getPurchases(req, res) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
     try {
-      const products = await db.collection("buyers").find({}).toArray();
+      const user = await db.collection('sessions').findOne({ token })
+      const products = await db.collection("buyers").find({ userId: user.userId }).toArray();
       res.status(200).send(products);
     } catch (error) {
       res.status(500).send(error);
